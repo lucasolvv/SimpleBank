6d0f36a80d23849017a43e9dc998cfb0a3ec2ac0
Lucas <lvksounds@gmail.com>
2025-04-24 16:45:48 -0300

    Chore: adicionando projeto para classes de comunicação http

---
+++ SimpleBank.Communication/Requests/RequestRegisterUserJson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBank.Communication.Requests
+{
+    public class RequestRegisterUserJson
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string CpfCnpj { get; set; } = string.Empty;
+        public string AccountType { get; set; } = string.Empty;
+    }
+}
---
+++ SimpleBank.Communication/Responses/ResponseRegisterUserJson.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleBank.Communication.Responses
+{
+    public class ResponseRegisterUserJson
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
