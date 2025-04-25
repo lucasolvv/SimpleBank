7bf5072f008c884c6acc036b3057c8c0262cfc23
Lucas <lvksounds@gmail.com>
2025-04-25 16:28:37 -0300

    fix: corrigindo nome das classes

---
+++ SimpleBank.Communication/Requests/RequestCreateUserJson.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleBank.Communication.Requests
+{
+    public class RequestCreateUserJson
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Document { get; set; } = string.Empty;
+        public string AccountType { get; set; } = string.Empty;
+    }
+}
