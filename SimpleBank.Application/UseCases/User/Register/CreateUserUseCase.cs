739dbf37d2cc831129f5bb7f3f29091220c5faf3
Lucas <lvksounds@gmail.com>
2025-04-25 16:31:03 -0300

    Remover UserSevice e adicionar validação de usuário
    O arquivo `UserSevice.cs` foi removido, eliminando todas as importações e a definição da classe.
    
    No arquivo `SimpleBank.Application.csproj`, foram adicionadas referências a novos pacotes, incluindo `Extensions.FluentValidation.Br`, `FluentValidation` e `Microsoft.Extensions.Identity.Core`, além de uma referência ao projeto `SimpleBank.Communication`.
    
    O arquivo `CreateUserUseCase.cs` agora contém a implementação da classe `CreateUserUseCase`, que inclui um método `Execute` para validar e criar um usuário, juntamente com um método privado `Validate` que utiliza um validador.
    
    Foi adicionado o arquivo `CreateUserValidator.cs`, que define regras de validação para os campos `Name`, `Password`, `Email` e `Document` do objeto `RequestCreateUserJson`.

---
+++ SimpleBank.Application/UseCases/User/Register/CreateUserUseCase.cs
@@ -0,0 +1,40 @@
+﻿using SimpleBank.Communication.Requests;
+using SimpleBank.Communication.Responses;
+using SimpleBank.Application.Services;
+using SimpleBank.Application.Services.Criptography;
+namespace SimpleBank.Application.UseCases.User.Register
+{
+    public class CreateUserUseCase
+    {
+        public ResponseCreateUserJson Execute(RequestCreateUserJson request)
+        {
+            Validate(request);
+
+            request.Password = PasswordEncripter.Encrypt(request.Password);
+            
+            // relacionar o user com a entidade do banco
+
+            // criar o user no banco
+
+
+            return new ResponseCreateUserJson
+            {
+                Nome = request.Name,
+                Email = request.Email
+            };
+        }
+
+        private void Validate(RequestCreateUserJson request)
+        {
+            var validator = new CreateUserValidator();
+            
+            var result = validator.Validate(request);
+
+            if (!result.IsValid)
+            {
+                var errors = result.Errors.Select(e => e.ErrorMessage).ToList();
+                throw new Exception(string.Join(", ", errors));
+            }
+        }
+    }
+}
