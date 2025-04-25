83ad189a30dc5465b381394710ab947eac459878
Lucas <lvksounds@gmail.com>
2025-04-25 12:44:39 -0300

    fix: tinha feito cagada na estrutura de pastas e quebrei o projeto. Concertei, corrigindo e subindo os projetos de infra e app que estavam faltando atualizar.

---
+++ SimpleBank.Infra/DependecyInjectionExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleBank.Infra
+{
+    public static class DependecyInjectionExtension
+    {
+        public static IServiceCollection AddInfraStructure(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        //private void AddDbContext(IServiceCollection services)
+        //{
+
+        //}
+
+        //private void AddRepositories(IServiceCollection services)
+        //{
+
+        //}
+    }
+}
