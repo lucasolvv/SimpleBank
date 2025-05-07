0fd3e817cb6c07e87e8f2c01a7fb7baf6e669b77
Lucas <lvksounds@gmail.com>
2025-05-07 14:38:46 -0300

    feat(exceptions): ✨ Adiciona classes de exceção personalizadas
    * Foram adicionadas as classes `ErrorOnValidationException` e `SimpleBankExceptions` no namespace `SimpleBank.Exceptions.ExceptionsBase`.
    * `ErrorOnValidationException` herda de `SimpleBankExceptions` e contém uma lista de erros do tipo `Exception`.
    * `SimpleBankExceptions` estende a classe base `Exception`, permitindo a criação de exceções personalizadas com mensagens e exceções internas.
    * Atualização do arquivo de solução (`SimpleBank.sln`) para incluir o projeto `SimpleBank.Exceptions`, com suas configurações de build e aninhamento.

---
+++ SimpleBank.Exceptions/ExceptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleBank.Exceptions.ExceptionsBase
+{
+    public class ErrorOnValidationException : SimpleBankExceptions
+    {
+        public List<Exception> error { get; set; } = new List<Exception>();
+
+        public ErrorOnValidationException(string message) : base(message)
+        {
+            error.Add(new Exception(message));
+        }
+    }
+}
---
+++ SimpleBank.Exceptions/ExceptionsBase/SimpleBankExceptions.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleBank.Exceptions.ExceptionsBase
+{
+    public class SimpleBankExceptions : Exception
+    {
+        public SimpleBankExceptions(string message) : base(message)
+        {
+        }
+        public SimpleBankExceptions(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+    }
+}
