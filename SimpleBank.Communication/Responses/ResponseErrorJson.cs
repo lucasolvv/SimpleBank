5641b8a3d1d0578a55bd60cec0935332e023deb7
Lucas <lvksounds@gmail.com>
2025-05-08 16:20:14 -0300

    feat(ResponseErrorJson): ✨ Adiciona classe de erro de resposta
    * Adicionadas novas importações e a classe `ResponseErrorJson`.
    * A classe possui uma propriedade `Errors`, que é uma lista de strings.
    * Inclui dois construtores: um para lista de erros e outro para mensagem única.

---
+++ SimpleBank.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBank.Communication.Responses
+{
+    public class ResponseErrorJson
+    {
+        public IList<string> Errors { get; set; }
+
+        public ResponseErrorJson(IList<string> errors) => Errors = errors;
+
+        public ResponseErrorJson(string errorMessage) => Errors = new List<string> { errorMessage };
+    }
+}
