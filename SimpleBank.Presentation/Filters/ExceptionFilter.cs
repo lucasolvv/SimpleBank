538d910541fa94850e78eecbda1bed00d9b67fb1
Lucas <lvksounds@gmail.com>
2025-05-08 16:21:50 -0300

    feat(filters): ✨ implementar tratamento de exceções
    * Implementada a interface `IExceptionFilter` na classe `ExceptionFilter`.
    * Adicionados métodos para tratar exceções específicas e genéricas.
    * O método `OnException` agora verifica se a exceção é do tipo `SimpleBankExceptions` e chama o método apropriado.
    * Tratamento de exceções específicas define o status HTTP como 400 (Bad Request) e retorna mensagens de erro.
    * Tratamento de exceções genéricas define o status como 500 (Internal Server Error) com uma mensagem de erro genérica.

--- SimpleBank.Presentation/Filters/ExceptionFilter.cs
+++ SimpleBank.Presentation/Filters/ExceptionFilter.cs
@@ -1,6 +1,31 @@
-﻿namespace SimpleBank.Presentation.Filters
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using SimpleBank.Communication.Responses;
+using SimpleBank.Exceptions.ExceptionsBase;
+using System.Net;
+
+namespace SimpleBank.Presentation.Filters
 {
-    public class ExceptionFilter
+    public class ExceptionFilter : IExceptionFilter
     {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is SimpleBankExceptions) HandleProjectExceptions(context);
+            else HandleGenericExceptions(context);
+        }
+
+        private void HandleProjectExceptions(ExceptionContext context)
+        {
+            var exception = context.Exception as ErrorOnValidationException; // cast da exception
+            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest; // define o status 
+            context.Result = new BadRequestObjectResult(new ResponseErrorJson(exception.ErrorMessages));
+        }
+
+        private void HandleGenericExceptions(ExceptionContext context)
+        {
+            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError ; // define o status 
+            context.Result = new ObjectResult(new ResponseErrorJson("Unknow error."));
+        }
     }
 }
+    
