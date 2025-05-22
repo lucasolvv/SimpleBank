8db1c81a694952e01994ec59a2e63abd373b373a
Lucas Oliveira <80175682+lucasolvv@users.noreply.github.com>
2025-05-22 16:33:06 -0300

    Revert "Feat/create user unit tests"

--- SimpleBank.Presentation/Controllers/UserController.cs
+++ SimpleBank.Presentation/Controllers/UserController.cs
@@ -15,10 +15,10 @@ namespace SimpleBank.Presentation.Controllers
 
         [HttpPost("/register")]
         [ProducesResponseType(typeof(ResponseCreateUserJson), StatusCodes.Status201Created)]
-        public async Task<IActionResult> Resgister([FromBody]RequestCreateUserJson request, [FromServices]ICreateUserUseCase useCase)
+        public IActionResult Resgister([FromBody]RequestCreateUserJson request, [FromServices]ICreateUserUseCase useCase)
         {
-            var result = await useCase.Execute(request);
-            return Created(string.Empty, result);
+            useCase.Execute(request);
+            return Created();
         }
 
         [HttpGet("/getuser/{email}")]
